85b8ae820ccb86363bf32ed624680612a737211f
sami peralahti <sami.peralahti@vainu.io>
2018-08-21 12:51:55 -0700

    got the css working

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+// const Header = () => <h2>Header</h2>;
+const Landing = () => <h2>Landing</h2>;
+const Photo = () => <h2>Photo</h2>;
 
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route path="/photo" component={Photo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDom from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 
+import "materialize-css";
+import "materialize-css/dist/css/materialize.min.css";
+
 import App from "./components/App";
 import reducers from "./reducers";
 
