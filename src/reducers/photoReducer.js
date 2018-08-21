7ceb94fae9b14d0c56c9fa983cda9ca264d1dc15
sami peralahti <sami.peralahti@vainu.io>
2018-08-21 15:12:33 -0700

    transforming the data to be in a better format for the store

--- src/reducers/photoReducer.js
+++ src/reducers/photoReducer.js
@@ -1,6 +1,15 @@
-export default function(state = {}, action) {
-  console.log(action);
+import { FETCH_PHOTOS } from "../actions/types";
+export default function(state = null, action) {
   switch (action.type) {
+    case FETCH_PHOTOS:
+      //   transform the data for the redux store
+      const objWithIdAsKey = action.payload.reduce((previous, currentPhoto) => {
+        // pull off the id propery and use it as a key
+        const { id, ...photoNoId } = currentPhoto;
+        previous[id] = photoNoId;
+        return previous;
+      }, {});
+      return objWithIdAsKey;
     default:
       return state;
   }
