eaf4de501940d48b3b1cd30cb13feba9c81979ac
sami peralahti <sami.peralahti@vainu.io>
2018-08-21 16:29:33 -0700

    added navigation links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
+          <Link to={"/"} className="brand-logo">
             Photo Collection
-          </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Sass</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li>
-          </ul>
+          </Link>
         </div>
       </nav>
     );
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,31 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
   renderCards(photos) {
-    return Object.keys(photos).map(key => {
-      const photoData = photos[key];
-      const { thumbnailUrl, title } = photoData;
-      return (
-        <div key={key} className="col s12 m7">
-          <div className="card">
-            <div className="card-image">
-              <img src={thumbnailUrl} />
-              <span className="card-title">{title}</span>
-            </div>
-            <div className="card-action">
-              <a href="#">This is a link</a>
-            </div>
+    return (
+      <div className="row">
+        {Object.keys(photos).map(key => {
+          const photoData = photos[key];
+          return this.renderCard(photoData, key);
+        })}
+      </div>
+    );
+  }
+
+  renderCard({ thumbnailUrl, title }, key) {
+    return (
+      <div key={key} className="col s4 m4">
+        <div className="card">
+          <div className="card-image">
+            <img src={thumbnailUrl} />
+            <span className="card-title">{title}</span>
+          </div>
+          <div className="card-action">
+            <Link to="/photo">Check Details</Link>
           </div>
         </div>
-      );
-    });
+      </div>
+    );
   }
 
   render() {
     const { photos } = this.props;
     if (!photos) {
-      return <div>Loading</div>;
+      return (
+        <div className="progress">
+          <div className="indeterminate" />
+        </div>
+      );
     }
 
     return this.renderCards(photos);
